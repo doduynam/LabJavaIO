dd3ccf0e880bf3c354a84f20e508a62d032ce2fe
doduynam <39905671+doduynam@users.noreply.github.com>
2022-03-12 21:35:56 +0700

    add somethign

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Student std = new Student();
+
+        try {
+            ObjectOutputStream outfile = new ObjectOutputStream(new FileOutputStream("student.bin"));
+            outfile.writeObject(std);
+            outfile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Done");
+
+
     }
 }
